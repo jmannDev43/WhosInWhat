b070dd6f1dfef96e8c628ded35f5a72f8627dee5
Josh Mann <jmann5443@gmail.com>
2016-01-18 16:21:35 -0600

    got network setup working (for the most part).  ran into issues trying to dynamically load node pictures. will work on this/labeling

--- client/search/selection.js
+++ client/search/selection.js
@@ -16,5 +16,24 @@ Template.selection.helpers({
     },
     tvShowsExist: function(){
         return SelectedItems.find({ mediaType: 'tv' }).count() > 0;
+    },
+    hasSelectedItems: function(){
+        return SelectedItems.find().count() > 0;
+    }
+});
+
+Template.selection.events({
+    'click .createNetwork': function(e, template){
+        // get data from SelectedItems collection
+
+//        loadInitialNetwork();
+        Meteor.call('loadInitialNetwork', function(err, resp){
+           if (!err){
+               Router.go('viewNetwork');
+           }
+        });
+
+        // make lazy-loading requests for each selected item (in a way that allows for expand/collapse)
+
     }
 });
